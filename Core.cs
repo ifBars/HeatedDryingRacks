e93603658ede341971885b2468709803fa67ec39
Haze4269 <tristens5465@gmail.com>
2025-12-26 00:10:11 -0700

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using S1API;
+using HeatedDryingRacks.Integrations;
+using HeatedDryingRacks.Utils;
+
+[assembly: MelonInfo(typeof(HeatedDryingRacks.Core), Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_AUTHOR)]
+[assembly: MelonGame(Constants.Game.GAME_STUDIO, Constants.Game.GAME_NAME)]
+
+namespace HeatedDryingRacks
+{
+    public class Core : MelonMod
+    {
+        public static Core? Instance { get; private set; }
+
+        public override void OnInitializeMelon()
+        {
+            Instance = this;
+            HarmonyPatches.SetModInstance(this);
+        }
+
+        public override void OnApplicationQuit()
+        {
+            Instance = null;
+        }
+    }
+}
